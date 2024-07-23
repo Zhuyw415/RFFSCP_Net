93bf9d4bd9b336d183baf8e38253dcd991a9510b
Zhuyw415 <zyw15862695937@163.com>
2024-07-23 15:36:29 +0800

    Delete RFFSCP/fcos directory

--- RFFSCP/fcos/__init__.py
+++
@@ -1,3 +0,0 @@
-from fcos import modeling
-
-__version__ = "0.1.1"
--- RFFSCP/fcos/modeling/backbone/__init__.py
+++
@@ -1,2 +0,0 @@
-from .fpn import build_fcos_resnet_fpn_backbone
-from .vovnet import build_vovnet_fpn_backbone, build_vovnet_backbone
